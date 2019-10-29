30ae3bb12a86c32a21bdc1dfcea14c1a44cfc979
nukyma <spanglishana@gmail.com>
2019-10-29 22:44:54 +0100

    Comentarios incluídos

---
+++ ejercicios/t3_invert.c
@@ -0,0 +1,12 @@
+// EJERCICIOS PROPUESTOS - TEMA 3 - INVERT
+/*
+Programa que muestre todos los argumentos que se le pasan al revés:
+	$ ./invert es cara la cacatua
+	$ autacac al arac se
+*/
+
+int main(int argc, char *argv[])
+{
+
+
+}
