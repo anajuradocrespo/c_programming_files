2e6c5651036072c2fb264842471b7cdd07ffe994
ana <am.juradoc@alumnos.urjc.es>
2020-06-21 12:41:23 +0200

    head comentado

--- T3_Ejercicios/t3_head.c
+++ T3_Ejercicios/t3_head.c
@@ -12,20 +12,30 @@ fgets(buf, 1024, stdin);
 #include <stdio.h>
 #include <stdlib.h>
 
+
 int main(int argc, char *argv[])
-{
-	int lineas;
-	char buf[1024];
-  	int cuenta = 0;
+{	
+	// VARIABLES
+
+
+	int lineas;			// variable entero llamado lineas. Número de líneas que hay que mostrar
+	char buf[1024];		// variable array de chars con 1024 posiciones llamado buf
+  	int cuenta = 0;		// variable entero llamado cuenta inicializado a 0
 	
+	// CONTROLAMOS EL NUMERO DE ARGUMENTOS QUE LLEGAN 
+
+	// Si no so se especifica en la llamada el numero de líneas, por defecto vamos a mostrar 10
 	if(argc == 1)
 	{
 		lineas = 10;
 	}
+	// Si se especifica el numero de líneas que hay que mostrar
 	else if (argc == 2)
-	{
-		char *num = argv[1] + 1;
-		lineas = atoi(num);		
+	{	
+		lineas = atoi(num);			// Convertimos el character que nos ha llegado por parametro en integer
+		printf("%s\n", argv[1] + 1 );
+		char *num = argv[1] + 1; 	// puntero a character llamado num que es al número 
+		
 	}
 	else
 	{
